a93709ef30acd3f0a7d7558edf0a74ddfc799b54
angelsl <angelsl@in04.sg>
2019-10-10 12:58:57 +0800

    Fix updater if given relative path

--- build-updater.sh
+++ build-updater.sh
@@ -11,10 +11,11 @@ cp source3{,alt} "$ULDIR/bin/"
 pushd "$TMPDIR"
 cat <<'EOF' > ev3-source-update.sh
 #!/bin/bash
+SRCFILE=$(realpath "$0")
 cd /
 rm -f /usr/local/bin/source3{,alt}
 rm -rf /usr/local/lib/node_modules
-sed -e '1,/^exit$/d' "$0" | base64 -d | tar -zxv
+sed -e '1,/^exit$/d' "$SRCFILE" | base64 -d | tar -zxv
 exit
 EOF
 tar --owner=0 --group=0 -zc usr | base64 >> ev3-source-update.sh
